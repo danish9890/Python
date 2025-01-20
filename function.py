1332a2d78f42e7f3c19bfe218bb6387544e13c1f
Danish Nadaf <137716814+danish9890@users.noreply.github.com>
2025-01-20 19:17:01 +0530

    Update function.py

--- function.py
+++ function.py
@@ -4,3 +4,11 @@ def sum(n):
     return sum(n-1)+n
 n=int(input("Enter the Number "))
 print(sum(n))
+
+def pattern(n):
+    if(n==0):
+        return 
+    print("*"*n)
+    pattern(n-1)
+n=int(input("enter the number "))
+pattern(n)
